5c9f969674cbd282f0b45b8e3edda0675fa05d8f
SeuZeRicardo <jstmotta@gmail.com>
2026-02-14 22:12:40 -0300

    feat: add loader to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Intro, Countdown, Cerimony, Presents } from "./components";
+import { Intro, Countdown, Cerimony, Presents, Loader } from "./components";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="app">
-      <Intro />
-      <Countdown />
-      <Cerimony />
-      <Presents />
+      <Loader />
+      <>
+        <Intro />
+        <Countdown />
+        <Cerimony />
+        <Presents />
+      </>
     </div>
   );
 }

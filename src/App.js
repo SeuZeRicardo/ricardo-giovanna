82dff4c706d610530e03b4a1730ac61d55d6119c
SeuZeRicardo <jstmotta@gmail.com>
2026-03-04 00:10:04 -0300

    feature: add responsive animation

--- src/App.js
+++ src/App.js
@@ -34,11 +34,71 @@ export default function App() {
 
   useGSAP(
     () => {
+      const matchMedia = gsap.matchMedia();
       if (isLoading) return;
       let containers = gsap.utils.toArray(".animation-section");
       let svgs = gsap.utils.toArray(".icons--side");
+      let leftSvgs = gsap.utils.toArray(".icons--side .left");
+      let rightSvgs = gsap.utils.toArray(".icons--side .right");
+      let leftFlower = gsap.utils.toArray(".animation-section #left-flower");
+      let rightFlower = gsap.utils.toArray(".animation-section #right-flower");
+      let titleSection = gsap.utils.toArray(".animation-section h2");
 
-      gsap.set(svgs, { autoAlpha: 0 });
+      // split text for section titles
+      let titleSplits = titleSection.map(
+        (el) => new SplitText(el, { type: "words,chars" }),
+      );
+
+      gsap.set([svgs, leftFlower, rightFlower, titleSection], { autoAlpha: 0 });
+
+      matchMedia.add("(min-width: 980px)", () => {
+        containers.forEach((container, idx) => {
+          gsap.set(leftSvgs[idx], { x: -32 });
+          gsap.set(rightSvgs[idx], { x: 32 });
+
+          gsap.timeline({
+            paused: true,
+            scrollTrigger: {
+              trigger: container,
+              start: "top center",
+              end: "bottom center",
+              toggleActions: "play reverse play reverse", //onEnter, onLeave, onEnterBack, onLeaveBack
+              onEnter: () => {
+                gsap.to(svgs[idx], {
+                  autoAlpha: 1,
+                  y: 0,
+                  duration: 0.6,
+                  stagger: 0.2,
+                });
+              },
+              onLeave: () => {
+                gsap.to(svgs[idx], {
+                  autoAlpha: 0,
+                  y: -20,
+                  duration: 0.6,
+                  stagger: 0.2,
+                });
+              },
+              onEnterBack: () => {
+                gsap.to(svgs[idx], {
+                  autoAlpha: 1,
+                  y: 0,
+                  duration: 0.6,
+                  stagger: 0.2,
+                });
+              },
+              onLeaveBack: () => {
+                gsap.to(svgs[idx], {
+                  autoAlpha: 0,
+                  y: 20,
+                  duration: 0.6,
+                  stagger: 0.2,
+                });
+              },
+            },
+          });
+        });
+      });
 
       containers.forEach((container, idx) => {
         gsap.timeline({
@@ -49,36 +109,95 @@ export default function App() {
             end: "bottom center",
             toggleActions: "play reverse play reverse", //onEnter, onLeave, onEnterBack, onLeaveBack
             onEnter: () => {
-              gsap.to(svgs[idx], {
+              // flowers
+              gsap.to([leftFlower[idx], rightFlower[idx]], {
                 autoAlpha: 1,
                 y: 0,
                 duration: 0.6,
                 stagger: 0.2,
               });
+
+              // title text animation
+              const split = titleSplits[idx];
+              if (split) {
+                gsap.to(titleSection[idx], {
+                  autoAlpha: 1,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+                gsap.to(split.chars, {
+                  autoAlpha: 1,
+                  y: 0,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+              }
             },
             onLeave: () => {
-              gsap.to(svgs[idx], {
+              gsap.to([leftFlower[idx], rightFlower[idx]], {
                 autoAlpha: 0,
                 y: -20,
                 duration: 0.6,
                 stagger: 0.2,
               });
+              const split = titleSplits[idx];
+              if (split) {
+                gsap.to(titleSection[idx], {
+                  autoAlpha: 0,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+                gsap.to(split.chars, {
+                  autoAlpha: 0,
+                  y: -20,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+              }
             },
             onEnterBack: () => {
-              gsap.to(svgs[idx], {
+              gsap.to([leftFlower[idx], rightFlower[idx]], {
                 autoAlpha: 1,
                 y: 0,
                 duration: 0.6,
                 stagger: 0.2,
               });
+              const split = titleSplits[idx];
+              if (split) {
+                gsap.to(titleSection[idx], {
+                  autoAlpha: 1,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+                gsap.to(split.chars, {
+                  autoAlpha: 1,
+                  y: 0,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+              }
             },
             onLeaveBack: () => {
-              gsap.to(svgs[idx], {
+              gsap.to([leftFlower[idx], rightFlower[idx]], {
                 autoAlpha: 0,
                 y: 20,
                 duration: 0.6,
                 stagger: 0.2,
               });
+              const split = titleSplits[idx];
+              if (split) {
+                gsap.to(titleSection[idx], {
+                  autoAlpha: 0,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+                gsap.to(split.chars, {
+                  autoAlpha: 0,
+                  y: 20,
+                  duration: 0.6,
+                  stagger: 0.02,
+                });
+              }
             },
           },
         });
@@ -106,7 +225,9 @@ export default function App() {
           <div className="animation-section">
             <Presents />
           </div>
-          <Footer />
+          <div className="animation-section special">
+            <Footer />
+          </div>
           <div className="icons--side">
             <Pan className="left anchor" />
             <Pequi className="right anchor" />
